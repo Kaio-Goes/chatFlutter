03389bd27466e2add643cd3034fdd98e5adfdadd
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-18 00:29:09 -0300

    Feature image in messages chat

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,45 +1,86 @@
+import 'dart:io';
+
 import 'package:chat/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  static const _defaultIImage = 'assets/images/avatar.png';
   final ChatMessage message;
   final bool belongsToCurrentUser;
 
   const MessageBubble(
       {super.key, required this.message, required this.belongsToCurrentUser});
 
+  Widget _showUserImage(String imageURL) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageURL);
+
+    if (uri.path.contains(_defaultIImage)) {
+      provider = const AssetImage(_defaultIImage);
+    } else if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: belongsToCurrentUser
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongsToCurrentUser
-                ? Colors.grey.shade300
-                : Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.all(Radius.circular(12)),
-          ),
-          width: 180,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: belongsToCurrentUser ? Colors.black : Colors.white),
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToCurrentUser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: belongsToCurrentUser
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  bottomRight: belongsToCurrentUser
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                ),
               ),
-              Text(
-                message.text,
-                style: TextStyle(
-                    color: belongsToCurrentUser ? Colors.black : Colors.white),
+              width: 180,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color:
+                            belongsToCurrentUser ? Colors.black : Colors.white),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                        color:
+                            belongsToCurrentUser ? Colors.black : Colors.white),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+        Positioned(
+            top: 0,
+            left: belongsToCurrentUser ? null : 165,
+            right: belongsToCurrentUser ? 165 : null,
+            child: _showUserImage(message.userImageURL)),
       ],
     );
   }
